9f11658697b686bcf9b69bd3a5dfb06c91b974d2
Ikechukwu Okalia <me@sirlouis.io>
2019-08-05 09:48:44 +0100

    error fix

--- app.init.php
+++ app.init.php
@@ -3,7 +3,7 @@
 $conf_path = \str_replace([
   "/vendor/7os/web-app",
   "/dev/7os/web-app",
-],"",__DIR__) . "/.system/appdata/7os/web-project";
+],"",__DIR__) . "/.system/appdata/7os/web-prjman";
 $conf_file = $conf_path . "/app.config";
 
 if (!\file_exists($conf_file) || !\is_readable($conf_file)) {
